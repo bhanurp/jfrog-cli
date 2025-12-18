e858e74722567b22ea455d37ff2faa221c09f762
Bhanu Reddy <bhanur@jfrog.com>
2025-12-18 23:42:32 +0530

    Revert "Fixed static check violation"
    This reverts commit b413bff2992b3643e443a8ff4b3e2c7615f15304.

--- nuget_test.go
+++ nuget_test.go
@@ -164,8 +164,9 @@ func testNugetCmd(t *testing.T, projectPath, buildName, buildNumber string, expe
 }
 
 // Add allow insecure connection for testings to work with localhost server
-func allowInsecureConnectionForTests(args *[]string) {
+func allowInsecureConnectionForTests(args *[]string) *[]string {
 	*args = append(*args, "--allow-insecure-connections")
+	return args
 }
 
 func assertNugetDependencies(t *testing.T, module buildInfo.Module, moduleName string) {
